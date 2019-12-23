a865be72ed696ae8ff20bf2d96bbe30556a261c6
Mike <mike.lee816@gmail.com>
2019-12-22 20:14:42 -0500

    Create to-do-container component

---
+++ src/components/to-do/to-do-container.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './to-do-container.styles.scss';
+
+const ToDoContainer = () => (
+    <div className='to-do-container'>
+        <h2>Todo 1</h2>
+        <h2>Todo 2</h2>
+        <h2>Todo 3</h2>
+    </div>
+);
+
+export default ToDoContainer;
