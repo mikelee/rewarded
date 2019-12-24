b63e6168f3cc2a6cf3f0913c2ef87dcb46a681bc
Mike <mike.lee816@gmail.com>
2019-12-24 18:30:33 -0500

    Create data file and populate data

--- src/components/to-do-container/to-do-container.component.jsx
+++ src/components/to-do-container/to-do-container.component.jsx
@@ -4,11 +4,11 @@ import './to-do-container.styles.scss';
 
 import ToDoItem from '../to-do-item/to-do-item.component';
 
+import data1 from '../../data';
+
 const ToDoContainer = () => (
     <div className='to-do-container'>
-        <ToDoItem text='To-Do 1' />
-        <ToDoItem text='To-Do 2' />
-        <ToDoItem text='To-Do 3' />
+        {data1.toDos.map(toDo => <ToDoItem text={toDo.text} />)}
     </div>
 );
 
