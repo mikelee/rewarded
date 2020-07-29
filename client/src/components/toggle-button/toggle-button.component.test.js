aa669fe86594da9e73e162593ca433864410ebe0
Mike <mike.lee816@gmail.com>
2020-07-29 15:51:29 -0400

    Add snapshot tests

---
+++ client/src/components/toggle-button/toggle-button.component.test.js
@@ -0,0 +1,19 @@
+import { shallow, configure }  from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallowToJson } from 'enzyme-to-json'
+import React from 'react';
+import ToggleButton from './toggle-button.component';
+
+configure({ adapter: new Adapter() });
+
+it('should render ToggleButton with CheckRoundedIcon', () => {
+    const wrapper = shallow(<ToggleButton type='forTodo' completed={1} toggleToDoCompleted={jest.fn()} />)
+
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+});
+
+it('should render Toggle Button without CheckRoundedIcon', () => {
+    const wrapper = shallow(<ToggleButton type='forTodo' completed={0} toggleToDoCompleted={jest.fn()} />)
+
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+})
