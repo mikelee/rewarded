ad9700bdcf663324ecb6f5c552ad7afb71d35a26
Mike <mike.lee816@gmail.com>
2021-02-25 20:36:17 -0500

    Create preloader component

---
+++ client/src/components/preloader/preloader.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './preloader.styles.scss';
+
+const Preloader = () => (
+    <div className='preloader'>
+        <div className='ripple'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+);
+
+export default Preloader;
