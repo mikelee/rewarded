4b6ba361bc6bfe4c3b967a4b595d056fb3426a82
Mike <mike.lee816@gmail.com>
2021-04-25 16:15:57 -0400

    Redesign Homepage and add a logout message

--- client/src/components/homepage/homepage.component.jsx
+++ client/src/components/homepage/homepage.component.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import Nav from '../nav/nav.component';
 
 import './homepage.styles.scss';
-import checklist from '../../assets/checklist.svg';
+import completed from '../../assets/completed.svg';
+import hiking from '../../assets/hiking.svg';
 
-const HomePage = () => (
+import { getLoggedOutMessage } from '../../redux/temporary/temporary.selectors';
+
+const HomePage = ({ currentUser, loggedOutMessage }) => (
     <div className='homepage'>
-        <div className='homepage-text-container'>
-            <h1 className='homepage-text'>Daily <br/> Achievements</h1>
-            <p className='homepage-subtext'>Achieve greatness!</p>
+        <Nav currentUser={currentUser} isTransparent={true} />
+        <div className='homepage-container'>
+            <div className='homepage-text-container'>
+                {
+                    !loggedOutMessage ?
+                        <>
+                            <h1 className='homepage-text'>Daily <br/> Achievements</h1>
+                            <p className='homepage-subtext'>Achieve greatness!</p>
+                        </>
+                    :
+                        <div>
+                            <h1 className='homepage-text'>You have successfully logged out</h1>
+                            <p className='homepage-subtext'>Enjoy your day!</p>
+                        </div>
+                }
+            </div>
+            <div className='graphic-container'>
+                {
+                    !loggedOutMessage ?
+                        <img className='homepage-graphic' src={completed} alt='checklist'/>
+                    :
+                        <img className='homepage-graphic' src={hiking} alt='hiking'/>
+
+                }
+            </div>
         </div>
-        <img className='homepage-graphic' src={checklist} alt='checklist'/>
     </div>
 );
 
-export default HomePage;
+const mapStateToProps = createStructuredSelector({
+    loggedOutMessage: getLoggedOutMessage
+});
+
+export default connect(mapStateToProps)(HomePage);
