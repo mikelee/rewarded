c12483781e91c7bc7409bf0b94fb2f43b9d01059
Mike <mike.lee816@gmail.com>
2021-10-03 15:55:15 -0400

    Update test snapshot

--- client/src/components/data-loader/data-loader.component.test.js
+++ client/src/components/data-loader/data-loader.component.test.js
@@ -1,4 +1,4 @@
-import { shallow,  configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import { shallowToJson } from 'enzyme-to-json';
 import React from 'react';
