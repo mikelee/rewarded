1daca2737be935302e1cf5dc0513a1783fbd1cc4
Mike Lee <mike.lee816@gmail.com>
2022-09-23 19:54:39 -0400

    Do not send index.html on all GET routes in production

--- server.js
+++ server.js
@@ -37,10 +37,6 @@ passportSetUp();
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-  
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
 }
 
 app.use('/', authRoutes);
