a01ce98ca877f193248b6cc629bef90ded1f62ac
Mike Lee <mike.lee816@gmail.com>
2022-09-28 21:47:37 -0400

    Create isTodoOwner() middleware

--- middleware.js
+++ middleware.js
@@ -1,3 +1,5 @@
+const connection = require('./db');
+
 const isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
@@ -7,7 +9,33 @@ const isLoggedIn = (req, res, next) => {
     }
 }
 
+const isTodoOwner = (req, res, next) => {
+    const todo_id = req.body.id;
+    const { user_id } = req.user;
+    
+    connection.query(`
+        SELECT EXISTS
+        (
+            SELECT *
+            FROM todos
+            WHERE todo_id = ? AND user_id = ?
+        )
+        as isOwner;
+    `, [todo_id, user_id], (err, result) => {
+        // isOwner may also be 0 if the todo_id doesn't exist
+        const isOwner = result[0].isOwner;
+
+        if (!isOwner) {
+            res.status(403);
+            return res.send('You are not authorized');
+        }
+
+        next();
+    });
+};
+
 module.exports = {
-    isLoggedIn
+    isLoggedIn,
+    isTodoOwner
 }
 
