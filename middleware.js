c3be42f420b83dab3e4b8e404effe28846339df7
Mike Lee <mike.lee816@gmail.com>
2022-09-30 18:31:33 -0400

    Refactor middleware to keep DRY. Create isRequirementOwner() middleware

--- middleware.js
+++ middleware.js
@@ -9,56 +9,59 @@ const isLoggedIn = (req, res, next) => {
     }
 }
 
-const isRewardOwner = (req, res, next) => {
-    const { reward_id } = req.body;
-    const { user_id } = req.user;
-    
-    connection.query(`
-        SELECT EXISTS
-        (
-            SELECT *
-            FROM rewards
-            WHERE reward_id = ? AND user_id = ?
-        )
-        as isOwner;
-    `, [reward_id, user_id], (err, result) => {
-        // isOwner may also be 0 if the todo_id doesn't exist
-        const isOwner = result[0].isOwner;
-        console.log(isOwner)
+const isOwnerQuery = (item, item_id, user_id) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`
+            SELECT EXISTS
+            (
+                SELECT *
+                FROM ${item}s
+                WHERE ${item}_id = ? AND user_id = ?
+            )
+            as doesExist;
+        `, [item_id, user_id], (err, result) => {
+            if (err) return reject(err);
+            // doesExist will be 0 if the item id doesn't exist or if the item id doesn't belong to the user
+            const isOwner = Boolean(result[0].doesExist);
 
-        if (!isOwner) {
-            res.status(403);
-            return forbidden(res);
-        }
-
-        next();
+            return resolve(isOwner);
+        });
     });
-};
+}
 
-const isTodoOwner = (req, res, next) => {
+const isRewardOwner = async (req, res, next) => {
+    const reward_id = req.body.reward_id !== undefined ? req.body.reward_id : req.query.reward_id;
+    const { user_id } = req.user;
+
+    const isRewardOwner = await isOwnerQuery('reward', reward_id, user_id);
+
+    if (!isRewardOwner) return forbidden(res);
+
+    next();
+}
+
+const isTodoOwner = async (req, res, next) => {
     const { todo_id } = req.body;
     const { user_id } = req.user;
-    
-    connection.query(`
-        SELECT EXISTS
-        (
-            SELECT *
-            FROM todos
-            WHERE todo_id = ? AND user_id = ?
-        )
-        as isOwner;
-    `, [todo_id, user_id], (err, result) => {
-        // isOwner may also be 0 if the todo_id doesn't exist
-        const isOwner = result[0].isOwner;
 
-        if (!isOwner) {
-            res.status(403);
-            return forbidden(res);
-        }
+    const isTodoOwner = await isOwnerQuery('todo', todo_id, user_id);
 
-        next();
-    });
-};
+    if (!isTodoOwner) return forbidden(res);
+
+    next();
+}
+
+const isRequirementOwner = async (req, res, next) => {
+    const { reward_id, todo_id } = req.body;
+    const { user_id } = req.user;
+
+    const isRewardOwner = await isOwnerQuery('reward', reward_id, user_id);
+    const isTodoOwner = await isOwnerQuery('todo', todo_id, user_id);
+
+    if (!isRewardOwner || !isTodoOwner) return forbidden(res);
+
+    next();
+}
 
 const forbidden = res => {
     res.status(403);
@@ -68,5 +71,6 @@ const forbidden = res => {
 module.exports = {
     isLoggedIn,
     isRewardOwner,
+    isRequirementOwner,
     isTodoOwner
 }
