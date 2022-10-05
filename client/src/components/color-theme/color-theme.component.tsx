00ffaa06fe355d2cb729f6e19b704fbbf2b9495a
Mike Lee <mike.lee816@gmail.com>
2022-10-04 21:11:10 -0400

    Move code to change color theme to its own function and export it

--- client/src/components/color-theme/color-theme.component.tsx
+++ client/src/components/color-theme/color-theme.component.tsx
@@ -1,20 +1,25 @@
 import { ReactElement, useEffect } from 'react';
+import { ColorTheme } from '../../../types';
 
 interface Props {
     children: ReactElement<any, any> | null
 }
 
+export const applyColorTheme = (colorName: ColorTheme | null) => {
+    if (colorName) {
+        document.body.style.setProperty('--color-primary', `rgb(var(--rgb-${colorName}))`);
+        document.body.style.setProperty('--color-primary-faded', `rgba(var(--rgb-${colorName}), .7)`);
+        document.body.style.setProperty('--color-primary-superfaded', `rgba(var(--rgb-${colorName}), .1)`);
+        document.body.style.setProperty('--color-primary-dark', `rgb(var(--rgb-${colorName}-dark))`);
+    }
+}
+
 const ColorTheme: React.FC<Props> = ({ children }) => {
 
     useEffect(() => {
-        const colorName: string = window.localStorage.colorTheme;
+        const colorName: ColorTheme = window.localStorage.colorTheme;
 
-        if (colorName) {
-            document.body.style.setProperty('--color-primary', `rgb(var(--rgb-${colorName}))`);
-            document.body.style.setProperty('--color-primary-faded', `rgba(var(--rgb-${colorName}), .7)`);
-            document.body.style.setProperty('--color-primary-superfaded', `rgba(var(--rgb-${colorName}), .1)`);
-            document.body.style.setProperty('--color-primary-dark', `rgb(var(--rgb-${colorName}-dark))`);
-        }
+        applyColorTheme(colorName);
     }, []);
 
     return children;
