6aebdff2b6068e259e3a300033e4bca499d4e350
Mike Lee <mike.lee816@gmail.com>
2023-01-11 15:23:40 -0500

    Add env variables
    password must be set in command line when starting cypress
    cypress_password=_____ npx cypress open

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    env: {
+      username: 'testUser',
+      password: ''
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
