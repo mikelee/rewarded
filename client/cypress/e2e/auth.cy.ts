1ded984359b6bcd1e31ccc1c6fb55a607f770111
Mike Lee <mike.lee816@gmail.com>
2023-01-11 17:17:56 -0500

    Use login command

--- client/cypress/e2e/auth.cy.ts
+++ client/cypress/e2e/auth.cy.ts
@@ -3,25 +3,6 @@ describe('auth tests', () => {
         const username = Cypress.env('username');
         const password = Cypress.env('password');
 
-        if (typeof password !== 'string' || !password) {
-            throw new Error('Missing password value, set using cypress_password=_____ in command line.');
-        }
-        
-        cy.visit('/');
-
-        cy.contains('Sign In').click();
-
-        cy.get('[name=username]')
-        .type(username)
-        .should('have.value', username);
-
-        cy.get('[name=password]')
-        .type(password, { log: false });
-
-        cy.get('button[type=submit]').click();
-
-        cy.contains(`Hi, ${username}`);
-        cy.contains('To Do');
-        cy.contains('Rewards');
+        cy.login(username, password);
     });
 });
