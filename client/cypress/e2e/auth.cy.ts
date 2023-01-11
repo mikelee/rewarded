4f40cfcacd84d1e6a7818c7c077eb23817576492
Mike Lee <mike.lee816@gmail.com>
2023-01-11 15:42:43 -0500

    Throw error if password wasn't set when starting cypress

--- client/cypress/e2e/auth.cy.ts
+++ client/cypress/e2e/auth.cy.ts
@@ -2,6 +2,10 @@ describe('auth tests', () => {
     it('logs in user' , () => {
         const username = Cypress.env('username');
         const password = Cypress.env('password');
+
+        if (typeof password !== 'string' || !password) {
+            throw new Error('Missing password value, set using cypress_password=_____ in command line.');
+        }
         
         cy.visit('/');
 
