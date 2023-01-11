c4c3980a47e9c81bfbb40b7202930dfcef916da0
Mike Lee <mike.lee816@gmail.com>
2023-01-11 17:16:57 -0500

    Create login command

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -35,3 +35,36 @@
 //     }
 //   }
 // }
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(username: string, password: string): Chainable<void>
+    }
+  }
+}
+
+Cypress.Commands.add('login' , (username, password) => {
+    if (typeof password !== 'string' || !password) {
+        throw new Error('Missing password value, set using cypress_password=_____ in command line.');
+    }
+    
+    cy.visit('/');
+
+    cy.contains('Sign In').click();
+
+    cy.get('[name=username]')
+    .type(username)
+    .should('have.value', username);
+
+    cy.get('[name=password]')
+    .type(password, { log: false });
+
+    cy.get('button[type=submit]').click();
+
+    cy.contains(`Hi, ${username}`);
+    cy.contains('To Do');
+    cy.contains('Rewards');
+});
+
+export {};
