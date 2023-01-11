54ec76247173859c62f8fbd79c2c1cb72e600d07
Mike Lee <mike.lee816@gmail.com>
2023-01-11 17:34:17 -0500

    Add test for logging out

--- client/cypress/e2e/auth.cy.ts
+++ client/cypress/e2e/auth.cy.ts
@@ -1,8 +1,18 @@
 describe('auth tests', () => {
-    it('logs in user' , () => {
-        const username = Cypress.env('username');
-        const password = Cypress.env('password');
+    const username = Cypress.env('username');
+    const password = Cypress.env('password');
 
+    it('logs in user' , () => {
         cy.login(username, password);
     });
+
+    it('logs out user' , () => {
+        cy.login(username, password);
+
+        cy.get('[data-testid=menu-button]').click();
+
+        cy.contains('Logout').click();
+
+        cy.contains('You have successfully logged out');        
+    })
 });
