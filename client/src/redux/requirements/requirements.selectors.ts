38873d772ca287dd68faf34889884448b6896888
Mike Lee <mike.lee816@gmail.com>
2023-02-14 19:16:36 -0500

    Create selector for the requirements that belong to a reward

--- client/src/redux/requirements/requirements.selectors.ts
+++ client/src/redux/requirements/requirements.selectors.ts
@@ -1,9 +1,19 @@
 import { createSelector } from 'reselect';
+import { Requirement } from '../../../types';
 import { ReduxState } from '../root-reducer';
+import { OwnProps } from '../../components/reward-item/reward-item.component';
 
 const selectRequirements = (state: ReduxState) => state.requirements;
 
 export const getRequirements = createSelector(
     [selectRequirements],
     requirements => requirements.requirements
+);
+
+export const getRewardRequirements = createSelector(
+    [
+        selectRequirements,
+        (state: ReduxState, props: OwnProps) => props
+    ],
+    (requirements, props) => requirements.requirements?.filter((requirement: Requirement) => requirement.rewardId === props.id)
 );
