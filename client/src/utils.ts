c61d1cb830abb170449695b59df7327b50142a9b
Mike Lee <mike.lee816@gmail.com>
2023-03-01 21:46:48 -0500

    Create quickSort function

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -21,3 +21,38 @@ export const fetchData = async (path: string, method: Method, body?: any ) => {
 
     return data;
 }
+
+function quickSort<T extends { text: string, timestamp: string }>(items: T[], compare: (item1: T, item2: T) => boolean, left: number = 0, right: number = items.length - 1) {
+    if (left < right) {
+        const partitionIndex = partition(items, left, right);
+
+        quickSort(items, compare, left, partitionIndex - 1);
+        quickSort(items, compare, partitionIndex + 1, right);
+    }
+
+    return items;
+
+    function partition(items: T[], left: number, right: number) {
+        const pivotItem = items[right];
+        // keeps track of index of last item that is ordered before pivot
+        let i = left - 1;
+
+        // scans for items to be placed before pivot and moves them to the beginning while updating i
+        for (let j = i + 1; j < right; j++) {
+            if (compare(items[j], pivotItem)) {
+                i++;
+                swap(items, i, j);
+            }
+        }
+
+        // place pivot where it belongs
+        swap(items, i + 1, right);
+        return i + 1;
+    }
+
+    function swap(items: T[], item1Index: number, item2Index: number) {
+        const temp = items[item1Index];
+        items[item1Index] = items[item2Index];
+        items[item2Index] = temp;
+    }
+}
