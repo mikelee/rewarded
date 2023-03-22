b25eeb5b87c44fff0b04f92597a3d0f9a8c5c54f
Mike Lee <mike.lee816@gmail.com>
2023-03-21 21:55:33 -0400

    Fix tests
    Changes:
    - add timestamp property to requirements
    - set requirements initial state to an empty array instead of null

--- client/src/redux/requirements/requirements.reducer.test.js
+++ client/src/redux/requirements/requirements.reducer.test.js
@@ -2,18 +2,18 @@ import requirementsReducer from './requirements.reducer';
 import { setRequirements } from './requirements.actions';
 
 const requirements = [
-    {rewardId: 1, todoId: 2, text: 'requirement 1', completed: 1},
-    {rewardId: 1, todoId: 3, text: 'requirement 2', completed: 1},
-    {rewardId: 1, todoId: 4, text: 'requirement 3', completed: 0}
+    {rewardId: 1, todoId: 2, text: 'requirement 1', completed: 1, timestamp: '2023-03-08T06:49:33.913Z'},
+    {rewardId: 1, todoId: 3, text: 'requirement 2', completed: 1, timestamp: '2023-03-08T06:49:43.318Z'},
+    {rewardId: 1, todoId: 4, text: 'requirement 3', completed: 0, timestamp: '2023-03-08T06:49:47.183Z'}
 ];
 
 it('should not set the requirements', () => {
-    expect(requirementsReducer(undefined, {})).toEqual({ requirements: null});
+    expect(requirementsReducer(undefined, {})).toEqual({ requirements: []});
 });
 
 it('should set the requirements', () => {
     const initialState = {
-        requirements: null
+        requirements: []
     };
 
     expect(requirementsReducer(initialState, setRequirements(requirements))).toEqual({ requirements: requirements});
