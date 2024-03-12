dd902dae493e3c824e6a2855c2bbd3480f13a89c
Mike Lee <mike.lee816@gmail.com>
2024-03-12 19:08:04 -0400

    Remove SSL option from postgres

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const postgres = require('postgres');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const sql = postgres(process.env.PG_URL, {
-    ssl: true
-});
+const sql = postgres(process.env.PG_URL);
 
 module.exports = sql;
