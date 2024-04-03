52b778eb61b6b0efc12062204938779dce3c1ab5
Mike Lee <mike.lee816@gmail.com>
2024-04-03 18:22:39 -0400

    Update react to v18
    Changes:
    - uses new root API

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -8,7 +8,10 @@ import './index.scss';
 import App from './App';
 import { store } from './redux/store';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <Provider store={store} >
         <BrowserRouter>
             <App />
